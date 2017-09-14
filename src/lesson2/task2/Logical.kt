529c89fb3bec7151a6b6e754c6afe411cac9629f
LexCorp1 <lr.romanov1999@mail.ru>
2017-09-14 16:43:30 +0300

    all lesson 2 done

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@ package lesson2.task2
 
 
 import lesson1.task1.sqr
+import java.lang.Math.abs
 import java.lang.Math.sqrt
 
 /**
@@ -32,7 +33,8 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+        x1==x2 || y1 == y2 || abs(x1-x2)==abs(y1-y2)
 
 /**
  * Средняя
@@ -42,14 +44,8 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean {
-    return if (r1==r2 && (x1!=x2 || y1!=y2)) false else
-        if (x1==x2 && y1==y2) true
-        else
-            if (r1<r2) {
-                x1+r1<=x2+r2 && x1-r1>=x2-r2 && y1+r1<=y2+r2 && y1-r1>=y2-r2 && r1<=r2-sqrt(sqr(x1-x2)+sqr(y1-y2))
-            } else x1+r1>=x2+r2 && x1-r1<=x2-r2 && y1+r1>=y2+r2 && y1-r1<=y2-r2 && r2<=r1-sqrt(sqr(x1-x2)+sqr(y1-y2))
-}
+                 x2: Double, y2: Double, r2: Double): Boolean = r1<=r2-sqrt(sqr(x1-x2)+sqr(y1-y2))
+
 
 /**
  * Средняя
