c06170ee3f25cb7083eb8d500ea1dd9c0d97002a
LexCorp1 <lr.romanov1999@mail.ru>
2017-09-20 03:00:54 +0300

    lesson 4

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -225,5 +225,6 @@ class Tests {
         assertEquals("двести тысяч два", russian(200002))
         assertEquals("девятьсот тысяч", russian(900000))
         assertEquals("двенадцать", russian(12))
+        assertEquals("пятьсот восемьдесят четыре тысячи пятьсот восемьдесят", russian(584580))
     }
 }
