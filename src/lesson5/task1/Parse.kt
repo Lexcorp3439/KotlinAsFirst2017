6eddf53314a63b01b2d5fbbd3f9b8c26554a993f
LexCorp1 <lr.romanov1999@mail.ru>
2017-10-04 00:59:12 +0300

    Parse

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -277,9 +277,8 @@ fun mostExpensive(description: String): String {
 val romanRegex = Regex("^[M]*(CM?|(DC{0,3})|CD?|C{0,3})?(XC?|(LX{0,3})|XL?|X{0,3})?(IX?|(VI{0,3})|IV?|I{0,3})?$")
 
 fun fromRoman(roman: String): Int {
-    return if (roman matches romanRegex) {
-        convertToRoman(roman)
-    } else -1
+    return if (!(roman matches romanRegex) || roman == "") -1
+    else convertToRoman(roman)
 }
 
 fun convertToRoman(str: String): Int {
